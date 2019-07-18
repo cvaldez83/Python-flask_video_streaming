78615f5da91c5bdd6cf003cd384d84f1bf7404d0
Cesar <cvaldez83@gmail.com>
2019-07-17 18:58:06 -0700

    added camera config parameters when initializing picamera

--- camera_pi.py
+++ camera_pi.py
@@ -12,9 +12,18 @@ class Camera(BaseCamera):
             time.sleep(2)
 
             stream = io.BytesIO()
+            camera.rotation = 180         #CV: added
+            camera.resolution = (640, 360) #CV: added
+            # camera.resolution = (1280, 720) #CV: added
+            # camera.framerate = 10          #CV: added
+            #camera.brightness = 60         #CV: added
+            #camera.contrast =  50          #CV: added
+            camera.exposure_mode = 'night'
+            # a=time.time()
             for _ in camera.capture_continuous(stream, 'jpeg',
                                                  use_video_port=True):
                 # return current frame
+                # print(time.time()-a)
                 stream.seek(0)
                 yield stream.read()
 
